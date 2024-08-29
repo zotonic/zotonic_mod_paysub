87d2cddc0a01fc01dcc1fefe00b9168fc9d5abf5
Marc Worrell <marc@worrell.nl>
2024-08-29 18:21:13 +0200

    Add extra export columns to subscription export

--- src/support/paysub_export_subscriptions.erl
+++ src/support/paysub_export_subscriptions.erl
@@ -40,28 +40,34 @@ fetch_data(Context) ->
 
 add_invoice(Sub, Context) ->
     #{
-        <<"psp">> := Psp,
-        <<"psp_customer_id">> := PspCustomerId
+        <<"rsc_id">> := UserId
     } = Sub,
-    Inv = fetch_last_invoice(Psp, PspCustomerId, Context),
-    maps:merge(Sub, Inv).
+    Inv = fetch_last_invoice_paid(UserId, Context),
+    Sub1 = maps:merge(Sub, Inv),
+    Sub1#{
+        <<"subscription_year">> => fetch_first_subscription_year(UserId, Context)
+    }.
 
-fetch_last_invoice(Psp, PspCustomerId, Context) ->
+
+fetch_last_invoice_paid(UserId, Context) ->
     case z_db:qmap_props_row("
-        select created as invoice_date,
-               payment_status as invoice_status,
-               currency as invoice_currency,
-               amount_due as invoice_amount_due,
-               amount_paid as invoice_amount_paid,
-               amount_remaining as invoice_amount_remaining,
-               props_json
-        from paysub_invoice
-        where psp = $1
-          and psp_customer_id = $2
-        order by created desc
+        select inv.created as invoice_date,
+               inv.payment_status as invoice_status,
+               inv.currency as invoice_currency,
+               inv.amount_due as invoice_amount_due,
+               inv.amount_paid as invoice_amount_paid,
+               inv.amount_remaining as invoice_amount_remaining,
+               inv.props_json
+        from paysub_invoice inv
+            join paysub_customer cust
+                on  inv.psp = cust.psp
+                and inv.psp_customer_id = cust.psp_customer_id
+        where inv.payment_status in ('paid', 'no_payment_required')
+          and cust.rsc_id = $1
+        order by inv.created desc
         limit 1
         ",
-        [ Psp, PspCustomerId ],
+        [ UserId ],
         Context)
     of
         {ok, #{ <<"items">> := [Item] } = Inv} ->
@@ -74,6 +80,18 @@ fetch_last_invoice(Psp, PspCustomerId, Context) ->
             #{}
     end.
 
+fetch_first_subscription_year(UserId, Context) ->
+    case z_db:q1("
+        select min(sub.started_at)
+        from paysub_subscription sub
+        where sub.rsc_id = $1
+        ",
+        [ UserId ],
+        Context)
+    of
+        {{Y, _, _}, _} -> Y;
+        undefined -> undefined
+    end.
 
 p(#{ <<"rsc_id">> := Id }, <<"country">>, Context) ->
     C = case m_rsc:p(Id, <<"billing_country">>, Context) of
@@ -86,18 +104,41 @@ p(#{ <<"rsc_id">> := Id }, <<"phone">>, Context) ->
         undefined -> m_rsc:p(Id, <<"phone_mobile">>, Context);
         V -> V
     end;
+p(#{ <<"rsc_id">> := Id }, <<"city">>, Context) ->
+    case m_rsc:p(Id, <<"billing_country">>, Context) of
+        undefined -> m_rsc:p(Id, <<"address_city">>, Context);
+        _ -> m_rsc:p(Id, <<"billing_city">>, Context)
+    end;
+p(#{ <<"rsc_id">> := Id }, <<"postcode">>, Context) ->
+    case m_rsc:p(Id, <<"billing_country">>, Context) of
+        undefined -> m_rsc:p(Id, <<"address_postcode">>, Context);
+        _ -> m_rsc:p(Id, <<"billing_postcode">>, Context)
+    end;
+p(#{ <<"rsc_id">> := Id }, <<"street">>, Context) ->
+    C = case m_rsc:p(Id, <<"billing_country">>, Context) of
+        undefined -> m_rsc:p(Id, <<"address_street_1">>, Context);
+        _ -> m_rsc:p(Id, <<"billing_street_1">>, Context)
+    end,
+    m_l10n:country_name(C, Context);
 p(#{ <<"rsc_id">> := Id }, <<"email">>, Context) ->
     case m_rsc:p(Id, <<"billing_email">>, Context) of
         undefined -> m_rsc:p(Id, <<"email">>, Context);
         V -> V
     end;
-p(#{ <<"rsc_id">> := Id }, <<"contact name">>, Context) ->
+p(#{ <<"rsc_id">> := Id }, <<"website">>, Context) ->
+    m_rsc:p(Id, <<"website">>, Context);
+p(#{ <<"rsc_id">> := Id }, <<"name_first">>, Context) ->
     MId = case m_edge:objects(Id, hasmaincontact, Context) of
         [Main|_] -> Main;
         [] -> Id
     end,
-    {Name, _} = z_template:render_to_iolist("_name.tpl", [ {id, MId} ], Context),
-    iolist_to_binary(Name);
+    m_rsc:p(MId, <<"name_first">>, Context);
+p(#{ <<"rsc_id">> := Id }, <<"name_surname">>, Context) ->
+    MId = case m_edge:objects(Id, hasmaincontact, Context) of
+        [Main|_] -> Main;
+        [] -> Id
+    end,
+    m_rsc:p(MId, <<"name_surname">>, Context);
 p(#{ <<"rsc_id">> := Id }, <<"contact country">>, Context) ->
     MId = case m_edge:objects(Id, hasmaincontact, Context) of
         [Main|_] -> Main;
@@ -126,6 +167,14 @@ p(#{ <<"rsc_id">> := Id }, <<"contact email">>, Context) ->
         undefined -> m_rsc:p(MId, <<"email">>, Context);
         V -> V
     end;
+p(#{ <<"rsc_id">> := Id }, <<"pref_language">>, Context) ->
+    MId = case m_edge:objects(Id, hasmaincontact, Context) of
+        [Main|_] -> Main;
+        [] -> Id
+    end,
+    m_rsc:p(MId, <<"pref_language">>, Context);
+p(#{ <<"price_amount">> := Amount, <<"price_currency">> := _Currency }, <<"price_amount">>, _Context) when is_integer(Amount) ->
+    Amount / 100;
 p(Data, P, _Context) ->
     maps:get(P, Data, undefined).
 
@@ -135,11 +184,17 @@ cols() ->
         <<"name">>,
         <<"email">>,
         <<"phone">>,
+        <<"street">>,
+        <<"city">>,
+        <<"postcode">>,
         <<"country">>,
-        <<"contact name">>,
+        <<"name_first">>,
+        <<"name_surname">>,
+        <<"pref_language">>,
         <<"contact email">>,
         <<"contact phone">>,
         <<"contact country">>,
+        <<"website">>,
         <<"psp">>,
         <<"psp_subscription_id">>,
         <<"status">>,
@@ -147,6 +202,7 @@ cols() ->
         <<"price_name">>,
         <<"price_currency">>,
         <<"price_amount">>,
+        <<"subscription_year">>,
         <<"started_at">>,
         <<"ended_at">>,
         <<"cancel_at">>,
